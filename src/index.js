125e1c5264e1e12be27dbe1094f3ec783bd71424
fyesinho <rodrigo.gonzalez.zura@gmail.com>
2022-12-29 15:22:08 -0300

    Update cert

--- src/index.js
+++ src/index.js
@@ -3,8 +3,8 @@ import fs from 'fs';
 import https from 'https';
 
 https.createServer({
-    cert: fs.readFileSync(require.resolve("./keys/fichadigital.crt")),
-    key: fs.readFileSync(require.resolve("./keys/apache.key")),
+    cert: fs.readFileSync(require.resolve("./keys/fullchain.pem")),
+    key: fs.readFileSync(require.resolve("./keys/privkey.pem")),
 }, app).listen(app.get('port'));
 
 console.log('init in', app.get('port'));
