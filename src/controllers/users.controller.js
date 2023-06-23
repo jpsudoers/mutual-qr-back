05c3dfff49fcebebd0c95339b4984e12bfd45aab
fyesinho <rodrigo.gonzalez.zura@gmail.com>
2023-06-23 12:32:58 -0400

    Update cert

--- src/controllers/users.controller.js
+++ src/controllers/users.controller.js
@@ -11,13 +11,33 @@ export const getUser = async (req, res) => {
     }
 }
 
+export const getBiByUserId = async (req, res) => {
+    const {id} = req.params;
+    try {
+        const pool = await getConnection();
+        const result = await pool.request().query(`exec [dbo].[Paneles_Usuarios] ${id}`);
+        res.json(result.recordset)
+    } catch (e) {
+        console.log(e)
+    }
+}
+
+export const getUserByNameOrRut = async (req, res) => {
+    const {reg} = req.params;
+    try {
+        const pool = await getConnection();
+        const result = await pool.request().query(`exec [dbo].[BusquedaRutNombre] '${reg}',1`);
+        res.json(result.recordset)
+    } catch (e) {
+        console.log(e)
+    }
+}
+
 export const postLogin = async (req, res) => {
     const {id, password} = req.body;
-    console.log(req.body)
     try {
         const pool = await getConnection();
         const result = await pool.request().query(`exec [dbo].[InicioSesion] '${id}','${password}'`);
-        console.log(result);
         if (result.recordset[0]['Respuesta'] === 'Usuario No Válido') {
             res.status(203).json('No autorizado');
         } else {
--- src/routes/users.route.js
+++ src/routes/users.route.js
@@ -1,9 +1,11 @@
 import {Router} from "express";
-import {getUser, postLogin} from "../controllers/users.controller";
+import {getBiByUserId, getUser, getUserByNameOrRut, postLogin} from "../controllers/users.controller";
 
 const router = Router();
 
 router.get('/user/:rut', getUser);
+router.get('/user/power-bi/:id', getBiByUserId);
+router.get('/user/search/:reg', getUserByNameOrRut);
 router.post('/login', postLogin);
 
 export default router;
